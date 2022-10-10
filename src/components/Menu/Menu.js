168a98736673398b7baf7b2a9ca330ef93596857
Rabbi Ahmed Limon <rabbiahemed>
2022-10-10 22:30:39 +0600

    Navbar

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { NavLink } from "react-router-dom";
+import "./Menu.css";
+const Menu = () => {
+  return (
+    <div className="menu fw-bold">
+      <Navbar bg="info" expand="lg">
+        <Container fluid>
+          <NavLink to="/" className="fs-1">
+            Learn<span style={{ color: "red" }}>Up</span>{" "}
+          </NavLink>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            ></Nav>
+            <div className="d-flex right-half">
+              <NavLink to="/topics">Topics</NavLink>
+              <NavLink to="/statistics">Statistics</NavLink>
+              <NavLink to="/blog">Blog</NavLink>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Menu;
